f23c5ee0b49d1e19ac3023876018daeb1c11f91f
Matt Damron <mattdamron@msn.com>
2020-12-13 14:21:12 -0700

    cleaned up code

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,13 +1,24 @@
+// setup URL's for openWeatherApi
 var openWeatherCurrentUrl = 'https://api.openweathermap.org/data/2.5/weather';
 var openWeatherForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 var openWeatherOneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall';
-var openWeatherUnits = '&units=imperial';
-var openWeatherUVI = 'https://api.openweathermap.org/data/2.5/uvi';
 var openWeatherIconUrl = 'https:///openweathermap.org/img/w/';
+
+// setup units and excludes
+var openWeatherUnits = '&units=imperial';
+var openWeatherExclude = '&exclude=hourly,minutely,alerts'
+var openWeatherUVI = 'https://api.openweathermap.org/data/2.5/uvi';
 var citySearch = '?q=herriman';
+
+// OpenAPIKey
 var openAPIKey = '&appid=199161a7849c135f147382ffb92ba10d';
+
+// date formating
 var dateFormat = 'MM/DD/YYYY';
-var cityObj = [];
+
+// Lat and Lon
+var currentLat = '';
+var currentLon = '';
 
 // get elements for the current weather
 var currentLocationEl = document.getElementById('currentLocation');
@@ -16,13 +27,17 @@ var currentTempEl = document.getElementById('currentTemp');
 var currentHumidityEl = document.getElementById('currentHumidity');
 var currentWindEl = document.getElementById('currentWind');
 var currentUVIEl = document.getElementById('currentUVI');
+var cityInputEl = document.getElementById('cityInput').value;
 
-// get elemet for the forecast weather
+// get element user-form
+var userFormEl = document.querySelector('#user-form');
+
+// get elemet for the forecast weather cards
 var forecastCardsEl = document.getElementById('forecast-cards');
 
-var getCurrentWeather = function () {
+var getCurrentWeather = function (event) {
+    event.preventDefault();
     var currentUrl = openWeatherCurrentUrl + citySearch + openWeatherUnits + openAPIKey;
-    // console.log(currentUrl);
     fetch(currentUrl)
         .then(function (currentResponse) {
             // request was successful
@@ -60,12 +75,14 @@ var getCurrentWeather = function () {
             currentWindEl.textContent = 'Wind Speed: ' + currentWind + ' MPH';
 
             // get current latitude and longitude
-            var currentLat = currentResponse.coord.lat;
-            var currentLon = currentResponse.coord.lon;
+            currentLat = '?lat=' + currentResponse.coord.lat;
+            currentLon = '&lon=' + currentResponse.coord.lon;
 
+        })
+        // get and set UV index
+        .then(function () {
             // set currentUVIUrl to fetch UV index
-            var currentUVIUrl = openWeatherUVI + '?lat=' + currentLat + '&lon=' + currentLon + openAPIKey;
-            // console.log(currentUVIUrl);
+            var currentUVIUrl = openWeatherUVI + currentLat + currentLon + openAPIKey;
             fetch(currentUVIUrl)
                 .then(function (currentUVIResponse) {
                     if (currentUVIResponse.ok) {
@@ -80,153 +97,68 @@ var getCurrentWeather = function () {
                     currentUVIEl.textContent = 'UV Index: ' + currentUVI;
                 })
         })
+        // get and set forecast
+        .then(function () {
+            var oneCallUrl = openWeatherOneCallUrl + currentLat + currentLon + openWeatherExclude + openWeatherUnits + openAPIKey;
+            fetch(oneCallUrl)
+                .then(function (openWeatherOneCallResponse) {
+                    // request was successful
+                    if (openWeatherOneCallResponse.ok) {
+                        return openWeatherOneCallResponse.json();
+                    } else {
+                        alert('Error: ' + openWeatherOneCallResponse.statusText);
+                    }
+                })
+                .then(function (openWeatherOneCallResponse) {
+                    for (var i = 1; i < openWeatherOneCallResponse.daily.length-2; i++) {
+
+                        // create forecast card
+                        var forcastCard = $('<div>')
+                            .addClass('card');
+
+                        // get and set forecast date header
+                        var convertedDate = moment.unix(openWeatherOneCallResponse.daily[i].dt).format(dateFormat);
+                        var forecastHeader = $('<div>')
+                            .addClass('card-header')
+                            .text(convertedDate);
+
+                        // get and set forecast icon
+                        var weatherIcon = openWeatherOneCallResponse.daily[i].weather[0].icon;
+                        var forecastIcon = $('<img>')
+                            .addClass('card-title')
+                            .attr('src', openWeatherIconUrl + weatherIcon + '.png')
+                            .attr('width', '100px')
+                            .attr('height', '100px');
+
+                        // get and set forecast temp
+                        var weatherTemp = Math.floor(openWeatherOneCallResponse.daily[i].temp.day);
+                        var forecastTemp = $('<p>')
+                            .addClass('card-text')
+                            .text('Temperature: ' + weatherTemp + ' ' + String.fromCharCode(176) + 'F');
+
+                        // get and set forecast humidity
+                        var weatherHumidity = openWeatherOneCallResponse.daily[i].humidity;
+                        var forecastHumidity = $('<p>')
+                            .addClass('card-text')
+                            .text('Humidity: ' + weatherHumidity + ' %');
+
+                        // append items to card
+                        forcastCard
+                            .append(forecastHeader)
+                            .append(forecastIcon)
+                            .append(forecastTemp)
+                            .append(forecastHumidity);
+
+                        // append card to card group
+                        $('#forecast-cards')
+                            .append(forcastCard);
+                    }
+                })
+        })
+
         .catch(function (error) {
             alert('Unable to connect to Weather API');
         });
 };
 
-var getForecastWeather = function () {
-    var forecastUrl = openWeatherForecastUrl + citySearch + openWeatherUnits + openAPIKey;
-    console.log(forecastUrl);
-    fetch(forecastUrl)
-        .then(function (forecastResponse) {
-            // request was successful
-            if (forecastResponse.ok) {
-                return forecastResponse.json();
-            } else {
-                alert('Error: ' + forecastResponse.statusText);
-            }
-        })
-        .then(function (forecastResponse) {
-            console.log(forecastResponse);
-            for (var i = 0; i < forecastResponse.list.length; i += 8) {
-                console.log(forecastResponse.list[i].dt_txt);
-                // var forecastDate = forecastResponse.dt_txt;
-                // var convertedDate = moment(forecastResponse.list[i].dt_txt).format(dateFormat);
-                // console.log(convertedDate);
-
-                // create forecast card
-                var forcastCard = $('<div>')
-                    .addClass('card');
-
-                // get and set forecast date header
-                var convertedDate = moment(forecastResponse.list[i].dt_txt).format(dateFormat);
-                console.log(convertedDate);
-                var forecastHeader = $('<div>')
-                    .addClass('card-header')
-                    .text(convertedDate);
-
-                // get and set forecast icon
-                var weatherIcon = forecastResponse.list[i].weather[0].icon;
-                console.log(weatherIcon);
-                var forecastIcon = $('<img>')
-                    .addClass('card-title')
-                    .attr('src', openWeatherIconUrl + weatherIcon + '.png')
-                    .attr('width', '100px')
-                    .attr('height', '100px');
-
-                // get and set forecast temp
-                var weatherTemp = Math.floor(forecastResponse.list[i].main.temp);
-                console.log(weatherTemp);
-                var forecastTemp = $('<p>')
-                    .addClass('card-text')
-                    .text('Temperature: ' + weatherTemp + ' ' + String.fromCharCode(176) + 'F');
-
-                // get and set forecast humidity
-                var weatherHumidity = forecastResponse.list[i].main.humidity;
-                console.log(weatherHumidity);
-                var forecastHumidity = $('<p>')
-                    .addClass('card-text')
-                    .text('Humidity: ' + weatherHumidity + ' %');
-
-                // append items to card
-                forcastCard
-                    .append(forecastHeader)
-                    .append(forecastIcon)
-                    .append(forecastTemp)
-                    .append(forecastHumidity);
-
-                // append card to card group
-                $('#forecast-cards')
-                    .append(forcastCard);
-            }
-        })
-        .catch(function (error) {
-            alert('Unable to connect to Weather API');
-        });
-};
-
-var oneCallApi = function () {
-    var openWeatherOneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely,alerts&appid=199161a7849c135f147382ffb92ba10d';
-    console.log(openWeatherOneCallUrl);
-    fetch(openWeatherOneCallUrl)
-        .then(function (openWeatherOneCallResponse) {
-            // request was successful
-            if (openWeatherOneCallResponse.ok) {
-                return openWeatherOneCallResponse.json();
-            } else {
-                alert('Error: ' + openWeatherOneCallResponse.statusText);
-            }
-        })
-        .then(function (openWeatherOneCallResponse) {
-            console.log(openWeatherOneCallResponse);
-            for (var i = 1; i < openWeatherOneCallResponse.daily.length; i++) {
-                // console.log(forecastResponse.list[i].dt_txt)
-                // var forecastDate = forecastResponse.dt_txt;
-                // var convertedDate = moment(forecastResponse.list[i].dt_txt).format(dateFormat);
-                // console.log(convertedDate);
-
-                // create forecast card
-                var forcastCard = $('<div>')
-                    .addClass('card');
-
-                // get and set forecast date header
-                var convertedDate = moment.unix(openWeatherOneCallResponse.daily[i].dt).format(dateFormat);
-                console.log(convertedDate);
-                var forecastHeader = $('<div>')
-                    .addClass('card-header')
-                    .text(convertedDate);
-
-                // get and set forecast icon
-                var weatherIcon = openWeatherOneCallResponse.daily[i].weather[0].icon;
-                console.log(weatherIcon);
-                var forecastIcon = $('<img>')
-                    .addClass('card-title')
-                    .attr('src', openWeatherIconUrl + weatherIcon + '.png')
-                    .attr('width', '100px')
-                    .attr('height', '100px');
-
-                // get and set forecast temp
-                var weatherTemp = Math.floor(openWeatherOneCallResponse.daily[i].temp.day);
-                console.log(weatherTemp);
-                var forecastTemp = $('<p>')
-                    .addClass('card-text')
-                    .text('Temperature: ' + weatherTemp + ' ' + String.fromCharCode(176) + 'F');
-
-                // get and set forecast humidity
-                var weatherHumidity = openWeatherOneCallResponse.daily[i].humidity;
-                console.log(weatherHumidity);
-                var forecastHumidity = $('<p>')
-                    .addClass('card-text')
-                    .text('Humidity: ' + weatherHumidity + ' %');
-
-                // append items to card
-                forcastCard
-                    .append(forecastHeader)
-                    .append(forecastIcon)
-                    .append(forecastTemp)
-                    .append(forecastHumidity);
-
-                // append card to card group
-                $('#forecast-cards')
-                    .append(forcastCard);
-            }
-        })
-        .catch(function (error) {
-            alert('Unable to connect to Weather API');
-        });
-};
-
-getCurrentWeather();
-// getForecastWeather();
-oneCallApi();
+userFormEl.addEventListener('submit', getCurrentWeather);
